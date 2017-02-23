cbe6fb22bae00f0113461f048be5cc465b74a5b9
Victor Dodon <Victor.Dodon@endava.com>
2017-02-23 10:41:47 +0200

    Add Main file

--- src/Library.go
+++ src/Library.go
@@ -168,28 +168,3 @@ func DeleteBook(w ResponseWriter, request *Request) {
 	w.WriteHeader(StatusOK)
 }
 
-func main() {
-	gorillaRoute := mux.NewRouter()
-	gorillaRoute.HandleFunc("/book", func(w ResponseWriter, request *Request) {
-		switch request.Method {
-
-		case MethodPost:
-			CreateBook(w, request)
-		case MethodGet:
-			GetBook(w, request)
-
-		}
-	})
-	gorillaRoute.HandleFunc("/book/{id:[0-9]+}", func(w ResponseWriter, request *Request) {
-		switch request.Method {
-
-		case MethodPut:
-			UpdateBook(w, request)
-		case MethodDelete:
-			DeleteBook(w, request)
-
-		}
-	})
-	Handle("/", gorillaRoute)
-	ListenAndServe(":8080", nil)
-}
